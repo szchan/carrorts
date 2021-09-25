86c03daf1b6bc013ed401f14acc28c833971963d
potato <77192700+Potato-like-coding@users.noreply.github.com>
2021-09-25 18:23:28 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int carrorts;
+    carrorts=25;
+    std::cout<<"I have "<<carrorts<<" carrorts."<<std::endl;
+    carrorts--;
+    std::cout<<"Now I have "<<carrorts<<" carrorts."<<std::endl;
+    printf("test\n");
+    printf("I have %d carrorts now");
+    return 0;
+}
